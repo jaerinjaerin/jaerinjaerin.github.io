29558c0e9038a2b4ed782ccf686cecfb70767b5e
leejaelll <jayaom91076@gmail.com>
2025-10-21 00:04:05 +0900

    fix: github 주소 수정

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -33,7 +33,7 @@ export function Header() {
         <div className='flex gap-2'>
           <ThemeSwitcher />
           <Button asChild variant='ghost' size='icon'>
-            <Link href='https://github.com/leejaelll' target='_blank'>
+            <Link href='https://github.com/jaerinjaerin' target='_blank'>
               <GithubIcon className='size-6' />
             </Link>
           </Button>
